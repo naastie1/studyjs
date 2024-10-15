03fef26739f9f90d3d996e39ca414ad4253ba3ce
Ana <example@gmail.com>
2024-10-15 15:40:13 +0900

    first example with movie

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello");
+"use strict";
+
+const watchMovie = +prompt("Сколько вы фильмов уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: watchMovie,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let a = prompt("Один из последних просмотренных фильмов?");
+let b = prompt("На сколько оцените его?");
+let c = prompt("Один из последних просмотренных фильмов?");
+let d = prompt("На сколько оцените его?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
