ad50006499ae73b386cfc549d8c1c34c82f59626
Ana <example@gmail.com>
2024-10-27 17:03:19 +0900

    second example

--- script.js
+++ script.js
@@ -10,13 +10,36 @@ const personalMovieDB = {
   privat: false,
 };
 
-let a = prompt("Один из последних просмотренных фильмов?");
-let b = prompt("На сколько оцените его?");
-let c = prompt("Один из последних просмотренных фильмов?");
-let d = prompt("На сколько оцените его?");
+// let a = prompt("Один из последних просмотренных фильмов?");
+// let b = prompt("На сколько оцените его?");
+// let c = prompt("Один из последних просмотренных фильмов?");
+// let d = prompt("На сколько оцените его?");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
 console.log(watchMovie);
 console.log(personalMovieDB);
+
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Один из последних просмотренных фильмов?");
+  let b = +prompt("На сколько оцените его?");
+
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("error!");
+}
